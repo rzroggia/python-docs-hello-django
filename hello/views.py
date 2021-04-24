ce4619a7ffa678fa3ca87183e7066b3e4b75e094
Rodrigo Roggia <44969154+rzroggia@users.noreply.github.com>
2021-04-24 09:11:30 -0300

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Surfando nas ondas da interwebs")
+    name = input("Nome?")
+    return HttpResponse("Bien venido", name)
