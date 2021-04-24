386dc13b91647799e81b13530a0a502a83c9fbc4
Rodrigo Roggia <44969154+rzroggia@users.noreply.github.com>
2021-04-24 08:43:08 -0300

    Update views.py
    Testing 2

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("E aí Gordão kkkk")
+    return HttpResponse("Surfando nas ondas da interwebs")
